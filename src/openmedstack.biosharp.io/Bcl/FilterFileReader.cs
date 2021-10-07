89ac9392540826901308b4f5e7d56492599516c7
Jacob Reimers <jacob@reimers.dk>
2021-10-07 10:23:08 +0200

    Add FilterFileReader.

---
+++ src/openmedstack.biosharp.io/Bcl/FilterFileReader.cs
@@ -0,0 +1,103 @@
+﻿namespace OpenMedStack.BioSharp.Io.Bcl
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /**
+ * Illumina uses an algorithm described in "Theory of RTA" that determines whether or not a cluster passes filter("PF") or not.
+ * These values are written as sequential bytes to Filter Files.  The structure of a filter file is as follows:
+ * Bytes 0-3  : 0
+ * Bytes 4-7  : unsigned int version
+ * Bytes 8-11 : unsigned int numClusters
+ */
+    public class FilterFileReader : IEnumerable<bool>
+    {
+        /** Number of bytes in the files header that will be skipped by the iterator*/
+        private const int HeaderSize = 12;
+
+        /** Expected Version */
+        public const int ExpectedVersion = 3;
+
+        /** Iterator over each cluster in the FilterFile */
+        private readonly Stream _bbIterator;
+
+        /** Version number found in the FilterFile, this should equal 3 */
+        public int Version;
+
+        /** The number of cluster's pf values stored in this file */
+        public long NumClusters { get; }
+
+        /** Byte representing a cluster failing filter(not a PF read), we test this exactly at
+     * the moment but technically the standard  may be to check only lowest significant bit */
+        private const byte FailedFilter = 0x00;
+
+        /** Byte representing a cluster passing filter(a PF read), we test this exactly at
+     * the moment but technically the standard  may be to check only lowest significant bit */
+        private const byte PassedFilter = 0x01;
+
+        /** The index of the current cluster within the file*/
+        private int _currentCluster;
+
+        public FilterFileReader(FileInfo file)
+        {
+            _bbIterator = File.Open(file.FullName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            // MMapBackedIteratorFactory.getByteIterator(HEADER_SIZE, file);
+            byte[] headerBuf = new byte[HeaderSize]; //bbIterator.getHeaderBytes();
+            var read = _bbIterator.Read(headerBuf);
+            if (read != HeaderSize)
+            {
+                throw new Exception("Invalid filter file");
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (headerBuf[i] != 0)
+                {
+                    throw new Exception(
+                        $"The first four bytes of a Filter File should be 0 but byte {i} was {headerBuf[i]} in file {file.FullName}");
+                }
+            }
+
+            Version = BitConverter.ToInt32(headerBuf.AsSpan(4, 4));
+            if (Version != ExpectedVersion)
+            {
+                throw new Exception(
+                    $"Expected version is {ExpectedVersion} but version found was {Version} in file {file.FullName}");
+            }
+
+            NumClusters =
+                BitConverter.ToUInt32(headerBuf.AsSpan(8, 4)); // UnsignedTypeUtil.uIntToLong(headerBuf.getInt());
+            if (_bbIterator.Length != NumClusters + HeaderSize)
+            {
+                throw new Exception($"Filter file size mismatch in file {file.FullName}");
+            }
+
+            _currentCluster = 0;
+        }
+
+        /// <inheritdoc />
+        public IEnumerator<bool> GetEnumerator()
+        {
+            while (_currentCluster < NumClusters)
+            {
+                byte value = (byte)_bbIterator.ReadByte();
+                _currentCluster += 1;
+                yield return value switch
+                {
+                    PassedFilter => true,
+                    FailedFilter => false,
+                    _ => throw new Exception($"Didn't recognized PF Byte (0x{value:X}) for element ({_currentCluster})")
+                };
+            }
+        }
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
---
+++ tests/openmedstack.biosharp.io.tests/Bcl/FilterFilesReaderTests.cs
@@ -0,0 +1,27 @@
+﻿namespace OpenMedStack.BioSharp.Io.Tests.Bcl
+{
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Io.Bcl;
+    using Xunit;
+
+    public class FilterFilesReaderTests
+    {
+        private readonly FilterFileReader _reader;
+
+        public FilterFilesReaderTests()
+        {
+            _reader = new FilterFileReader(new FileInfo(
+                @"data/illumina/25T8B8B25T/Data/Intensities/BaseCalls/L001/s_1_1101.filter"));
+        }
+
+        [Fact]
+        public void CanRead()
+        {
+            var amount = _reader.Count();
+
+            Assert.Equal(_reader.NumClusters, amount);
+        }
+    }
+}
