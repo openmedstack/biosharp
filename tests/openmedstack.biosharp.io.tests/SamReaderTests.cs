e48f50cba92575e76e70f61d6a0d38175b9d5db0
Jacob Reimers <jacob@reimers.dk>
2026-05-28 13:02:48 +0200

    Use raw strings

--- tests/openmedstack.biosharp.io.tests/SamReaderTests.cs
+++ tests/openmedstack.biosharp.io.tests/SamReaderTests.cs
@@ -12,15 +12,17 @@ public class SamReaderTests
     [Fact]
     public async Task CanReadFile()
     {
-        var file = @"@HD  VN:1.6  SO:coordinate
-@SQ  SN:ref  LN:45
-r001  99  ref  7  30  8M2I4M1D3M  =  37  39  TTAGATAAAGGATACTG  *
-r002  0  ref  9  30  3S6M1P1I4M  *  0  0  AAAAGATAAGGATA  *
-r003  0  ref  9  30  5S6M  *  0  0  GCCTAAGCTAA  *  SA:Z:ref,29,-,6H5M,17,0;
-r004  0  ref  16  30  6M14N5M  *  0  0  ATAGCTTCAGC  *
-r003  2064  ref  29  17  6H5M  *  0  0  TAGGC  *  SA:Z:ref,9,+,5S6M,30,1;
-r001  147  ref  37  30  9M  =  7  -39  CAGCGGCAT  *  NM:i:1
-".Replace("  ", "\t");
+        var file = """
+                   @HD  VN:1.6  SO:coordinate
+                   @SQ  SN:ref  LN:45
+                   r001  99  ref  7  30  8M2I4M1D3M  =  37  39  TTAGATAAAGGATACTG  *
+                   r002  0  ref  9  30  3S6M1P1I4M  *  0  0  AAAAGATAAGGATA  *
+                   r003  0  ref  9  30  5S6M  *  0  0  GCCTAAGCTAA  *  SA:Z:ref,29,-,6H5M,17,0;
+                   r004  0  ref  16  30  6M14N5M  *  0  0  ATAGCTTCAGC  *
+                   r003  2064  ref  29  17  6H5M  *  0  0  TAGGC  *  SA:Z:ref,9,+,5S6M,30,1;
+                   r001  147  ref  37  30  9M  =  7  -39  CAGCGGCAT  *  NM:i:1
+
+                   """.Replace("  ", "\t");
 
         var ms = new MemoryStream(Encoding.UTF8.GetBytes(file));
         await using var _ = ms;
@@ -33,15 +35,17 @@ r001  147  ref  37  30  9M  =  7  -39  CAGCGGCAT  *  NM:i:1
     [Fact]
     public async Task CanReadFile2()
     {
-        var file = @"@HD	VN:1.0	SO:coordinate
-@SQ	SN:1	LN:249250621	AS:NCBI37	UR:file:/data/local/ref/GATK/human_g1k_v37.fasta	M5:1b22b98cdeb4a9304cb5d48026a85128
-@SQ	SN:2	LN:243199373	AS:NCBI37	UR:file:/data/local/ref/GATK/human_g1k_v37.fasta	M5:a0d9851da00400dec1098a9255ac712e
-@SQ	SN:3	LN:198022430	AS:NCBI37	UR:file:/data/local/ref/GATK/human_g1k_v37.fasta	M5:fdfd811849cc2fadebc929bb925902e5
-@RG	ID:UM0098:1	PL:ILLUMINA	PU:HWUSI-EAS1707-615LHAAXX-L001	LB:80	DT:2010-05-05T20:00:00-0400	SM:SD37743	CN:UMCORE
-@RG	ID:UM0098:2	PL:ILLUMINA	PU:HWUSI-EAS1707-615LHAAXX-L002	LB:80	DT:2010-05-05T20:00:00-0400	SM:SD37743	CN:UMCORE
-@PG	ID:bwa	VN:0.5.4
-@PG	ID:GATK TableRecalibration	VN:1.0.3471	CL:Covariates=[ReadGroupCovariate, QualityScoreCovariate, CycleCovariate, DinucCovariate, TileCovariate], default_read_group=null, default_platform=null, force_read_group=null, force_platform=null, solid_recal_mode=SET_Q_ZERO, window_size_nqs=5, homopolymer_nback=7, exception_if_no_tile=false, ignore_nocall_colorspace=false, pQ=5, maxQ=40, smoothing=1
-".Replace("  ", "\t");
+        var file = """
+                   @HD	VN:1.0	SO:coordinate
+                   @SQ	SN:1	LN:249250621	AS:NCBI37	UR:file:/data/local/ref/GATK/human_g1k_v37.fasta	M5:1b22b98cdeb4a9304cb5d48026a85128
+                   @SQ	SN:2	LN:243199373	AS:NCBI37	UR:file:/data/local/ref/GATK/human_g1k_v37.fasta	M5:a0d9851da00400dec1098a9255ac712e
+                   @SQ	SN:3	LN:198022430	AS:NCBI37	UR:file:/data/local/ref/GATK/human_g1k_v37.fasta	M5:fdfd811849cc2fadebc929bb925902e5
+                   @RG	ID:UM0098:1	PL:ILLUMINA	PU:HWUSI-EAS1707-615LHAAXX-L001	LB:80	DT:2010-05-05T20:00:00-0400	SM:SD37743	CN:UMCORE
+                   @RG	ID:UM0098:2	PL:ILLUMINA	PU:HWUSI-EAS1707-615LHAAXX-L002	LB:80	DT:2010-05-05T20:00:00-0400	SM:SD37743	CN:UMCORE
+                   @PG	ID:bwa	VN:0.5.4
+                   @PG	ID:GATK TableRecalibration	VN:1.0.3471	CL:Covariates=[ReadGroupCovariate, QualityScoreCovariate, CycleCovariate, DinucCovariate, TileCovariate], default_read_group=null, default_platform=null, force_read_group=null, force_platform=null, solid_recal_mode=SET_Q_ZERO, window_size_nqs=5, homopolymer_nback=7, exception_if_no_tile=false, ignore_nocall_colorspace=false, pQ=5, maxQ=40, smoothing=1
+
+                   """.Replace("  ", "\t");
 
         var ms = new MemoryStream(Encoding.UTF8.GetBytes(file));
         await using var _ = ms;
--- tests/openmedstack.biosharp.io.tests/VcfFileTests.cs
+++ tests/openmedstack.biosharp.io.tests/VcfFileTests.cs
@@ -26,14 +26,16 @@ public class VcfFileTests
     [Fact]
     public async Task CanParseFile()
     {
-        var content = @"##fileformat=VCFv4.2
-##FORMAT=<ID=GT,Number=1,Type=Integer,Description=""Genotype"">
-##FORMAT=<ID=GP,Number=G,Type=Float,Description=""Genotype Probabilities"">
-##FORMAT=<ID=PL,Number=G,Type=Float,Description=""Phred-scaled Genotype Likelihoods"">
-#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	SAMP001	SAMP002
-20	1291018	rs11449	G	A	.	PASS	.	GT	0/0	0/1
-20	2300608	rs84825	C	T	.	PASS	.	GT:GP	0/1:.	0/1:0.03,0.97,0
-20	2301308	rs84823	T	G	.	PASS	.	GT:PL	./.:.	1/1:10,5,0";
+        var content = """
+                      ##fileformat=VCFv4.2
+                      ##FORMAT=<ID=GT,Number=1,Type=Integer,Description="Genotype">
+                      ##FORMAT=<ID=GP,Number=G,Type=Float,Description="Genotype Probabilities">
+                      ##FORMAT=<ID=PL,Number=G,Type=Float,Description="Phred-scaled Genotype Likelihoods">
+                      #CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	SAMP001	SAMP002
+                      20	1291018	rs11449	G	A	.	PASS	.	GT	0/0	0/1
+                      20	2300608	rs84825	C	T	.	PASS	.	GT:GP	0/1:.	0/1:0.03,0.97,0
+                      20	2301308	rs84823	T	G	.	PASS	.	GT:PL	./.:.	1/1:10,5,0
+                      """;
         var file = new MemoryStream(Encoding.UTF8.GetBytes(content));
         await using var _ = file;
         var reader = new VcfFileReader();
@@ -47,24 +49,26 @@ public class VcfFileTests
     [Fact]
     public void CanParseHeaders()
     {
-        var file = @"##fileformat=VCFv4.2
-##fileDate=20090805
-##source=myImputationProgramV3.1
-##reference=file:///seq/references/1000GenomesPilot-NCBI36.fasta
-##contig=<ID=20,length=62435964,assembly=B36,md5=f126cdf8a6e0c7f379d618ff66beb2da,species=""Homo sapiens"",taxonomy=x>
-##phasing=partial
-##INFO=<ID=NS,Number=1,Type=Integer,Description=""Number of Samples With Data"">
-##INFO=<ID=DP,Number=1,Type=Integer,Description=""Total Depth"">
-##INFO=<ID=AF,Number=A,Type=Float,Description=""Allele Frequency"">
-##INFO=<ID=AA,Number=1,Type=String,Description=""Ancestral Allele"">
-##INFO=<ID=DB,Number=0,Type=Flag,Description=""dbSNP membership, build 129"">
-##INFO=<ID=H2,Number=0,Type=Flag,Description=""HapMap2 membership"">
-##FILTER=<ID=q10,Description=""Quality below 10"">
-##FILTER=<ID=s50,Description=""Less than 50% of samples have data"">
-##FORMAT=<ID=GT,Number=1,Type=String,Description=""Genotype"">
-##FORMAT=<ID=GQ,Number=1,Type=Integer,Description=""Genotype Quality"">
-##FORMAT=<ID=DP,Number=1,Type=Integer,Description=""Read Depth"">
-##FORMAT=<ID=HQ,Number=2,Type=Integer,Description=""Haplotype Quality"">";
+        var file = """
+                   ##fileformat=VCFv4.2
+                   ##fileDate=20090805
+                   ##source=myImputationProgramV3.1
+                   ##reference=file:///seq/references/1000GenomesPilot-NCBI36.fasta
+                   ##contig=<ID=20,length=62435964,assembly=B36,md5=f126cdf8a6e0c7f379d618ff66beb2da,species="Homo sapiens",taxonomy=x>
+                   ##phasing=partial
+                   ##INFO=<ID=NS,Number=1,Type=Integer,Description="Number of Samples With Data">
+                   ##INFO=<ID=DP,Number=1,Type=Integer,Description="Total Depth">
+                   ##INFO=<ID=AF,Number=A,Type=Float,Description="Allele Frequency">
+                   ##INFO=<ID=AA,Number=1,Type=String,Description="Ancestral Allele">
+                   ##INFO=<ID=DB,Number=0,Type=Flag,Description="dbSNP membership, build 129">
+                   ##INFO=<ID=H2,Number=0,Type=Flag,Description="HapMap2 membership">
+                   ##FILTER=<ID=q10,Description="Quality below 10">
+                   ##FILTER=<ID=s50,Description="Less than 50% of samples have data">
+                   ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+                   ##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
+                   ##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Read Depth">
+                   ##FORMAT=<ID=HQ,Number=2,Type=Integer,Description="Haplotype Quality">
+                   """;
 
         var metaInformation =
             file.Split('\n').Select(x => x.Trim()).Select(x => VcfMetaReader.Read(x.AsSpan())).ToList();
